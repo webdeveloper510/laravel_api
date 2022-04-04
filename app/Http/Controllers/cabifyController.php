bfa87b4bd691ae7486cc8bedea307f7e28cae1f8
webdeveloper510 <developerweb6@gmail.com>
2022-04-05 01:07:32 +0530

    testing cabify api

--- app/Http/Controllers/cabifyController.php
+++ app/Http/Controllers/cabifyController.php
@@ -37,7 +37,7 @@ class cabifyController extends Controller
 
 
     function PostCreateDelivery(){
-        $curl = curl_init();
+      $curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://cabify-sandbox.com/api/v3/graphql',
@@ -48,14 +48,50 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS =>'{"query":"","variables":{"optimize":true,"senderId":"c432e92c224370bccf5715eae53ff94a","productId":"db10033ac9b52ac4e1d785107f3e96aa","deliveryPoints":[{"name":"PickUp point","instr":"https://url.example","addr":"Calle de Évora","num":"1","city":"Madrid","country":"Spain","loc":[40.3865045,-3.718262699999999],"receiver":{"mobileCc":"34","mobileNum":"666778899","name":"John Doe"}},{"name":"Destination point","addr":"Calle de Évora","num":"1","city":"Madrid","country":"Spain","loc":[40.3865045,-3.718262699999999],"receiver":{"mobileCc":"34","mobileNum":"666998877","name":"Jane Doe"}}]}}',
+  CURLOPT_POSTFIELDS =>'{"query":"mutation CreateDelivery($senderId: c432e92c224370bccf5715eae53ff94a, $productId: db10033ac9b52ac4e1d785107f3e96aa, $deliveryPoints: [{
+    "name":"PickUp point",
+    "instr":"https://url.example",
+    "addr":"Calle de Évora",
+    "num":"1",
+    "city":"Madrid",
+    "country":"Spain",
+    "loc":[
+       40.3865045,
+       -3.718262699999999
+    ],
+    "receiver":{
+       "mobileCc":"34",
+       "mobileNum":"666778899",
+       "name":"John Doe"
+    }
+ },
+ {
+    "name":"Destination point",
+    "addr":"Calle de Évora",
+    "num":"1",
+    "city":"Madrid",
+    "country":"Spain",
+    "loc":[
+       40.3865045,
+       -3.718262699999999
+    ],
+    "receiver":{
+       "mobileCc":"34",
+       "mobileNum":"666998877",
+       "name":"Jane Doe"
+    }
+ }
+], $optimize: true) {\\r\\n  createDelivery(deliveryInput: {senderId: c432e92c224370bccf5715eae53ff94a, productId: db10033ac9b52ac4e1d785107f3e96aa, deliveryPoints: $deliveryPoints, optimize: true}) {\\r\\n    sender {\\r\\n      id\\r\\n      name\\r\\n      email\\r\\n    }\\r\\n    id\\r\\n    deliveryPoints {\\r\\n      addr\\r\\n      city\\r\\n      receiver {\\r\\n        mobileCc\\r\\n        mobileNum\\r\\n        name\\r\\n      }\\r\\n      instr\\r\\n      loc\\r\\n      name\\r\\n      num\\r\\n    }\\r\\n    startAt\\r\\n    startType\\r\\n  }\\r\\n}","variables":{}}',
   CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json',
-    'Authorization:1763-011734-b6deedd5-4bdb-4a72-4da3-cf6f541f3bdf'
+    'Authorization: Bearer wn2iJLu8kkvmnKcM5vUncHeUTRW1eD',
+    'Content-Type: application/json'
   ),
 ));
 
 $response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
 
 if($data['id']){
       
@@ -69,7 +105,5 @@ if($data['id']){
   return $resp;
 }
 
-curl_close($curl);
-echo $response;
     }
 }
